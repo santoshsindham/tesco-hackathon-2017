d37f64e63f1266f9de62dd1425cdd313bbb1b6ae
Santosh Sindham <santoshkumar.sindham@in.tesco.com>
2017-09-21 19:22:40 +0530

    tesco games as a service inclusion

---
+++ tesco_gaas/src/main/java/hello/GamesController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GamesController {
+
+    @RequestMapping("/game")
+    public String game(@RequestParam(value="name", required=false, defaultValue="game1") String name, Model model) {
+        model.addAttribute("name", name);
+        return "game";
+    }
+
+}
