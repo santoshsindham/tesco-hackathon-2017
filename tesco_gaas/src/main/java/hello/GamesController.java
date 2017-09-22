1b9f5379e496c9b7a444c5c44649ebc5ba8a91e8
Santosh Sindham <santoshkumar.sindham@in.tesco.com>
2017-09-22 09:34:28 +0530

    renamed tictactoe game

--- tesco_gaas/src/main/java/hello/GamesController.java
+++ tesco_gaas/src/main/java/hello/GamesController.java
@@ -20,10 +20,10 @@ public class GamesController {
         return "snakeGame";
     }
 
-    @RequestMapping("/tictactoe")
+    @RequestMapping("/gaastictactoe")
     public String hostedgame(@RequestParam(value="name", required=false, defaultValue="game1") String name, Model model) {
         model.addAttribute("name", name);
-        return "tictactoe";
+        return "gaastictactoe";
     }
 
 }
